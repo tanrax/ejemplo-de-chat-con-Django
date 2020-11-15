e486ebd2d52c33e356d57ebb0d07dbd9a52c6538
Andros Fenollosa <andros@fenollosa.email>
2020-11-15 11:13:39 +0100

    Add database

--- mi_web/settings.py
+++ mi_web/settings.py
@@ -70,7 +70,6 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mi_web.wsgi.application'
 
 
 # Database
@@ -78,8 +77,12 @@ WSGI_APPLICATION = 'mi_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
